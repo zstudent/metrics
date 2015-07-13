fc8e7171bca81301da2634b96f8593eeb7afe06d
Zaal <Zaal_Lyanov@epam.com>
2015-07-13 15:19:54 +0400

    optimized (part 1)

--- SampleMetrics/src/metrics/Data.java
+++ SampleMetrics/src/metrics/Data.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Data implements Iterable<Record> {
 	
-	public static final int MAX = 1000000;
+	public static final int MAX = 10000000;
 	public static final int READ_SIZE = 300;
 	private static final Random random = new Random();
 	
@@ -35,8 +35,6 @@ public class Data implements Iterable<Record> {
 
 			@Override
 			public void remove() {
-				// TODO Auto-generated method stub
-				
 			}
 		};
 	}
--- SampleMetrics/src/metrics/Metrics.java
+++ SampleMetrics/src/metrics/Metrics.java
@@ -1,34 +1,94 @@
 package metrics;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Metrics {
 	
-	
+	public static final int MAX_PAIRS = 1000; 
+
 	public static void main(final String[] args) {
-		
+
+		System.out.println("Start");
+
+		long start = System.nanoTime();
+
 		Metrics metrics = new Metrics();
-		
+
 		metrics.doWork();
-		
+
+		long stop = System.nanoTime();
+		System.out.println("Finish");
+		System.out.println("Elapsed: " + (stop - start));
 	}
 
 	private void doWork() {
-		
+
 		Data data = new Data();
 		ReferenceSequence sequence = new ReferenceSequence();
+
+		final AtomicLong total = new AtomicLong(0);
+
+		// Iterator<Record> it = data.iterator();
+		//
+		// while (it.hasNext()) {
+		// Record record = it.next();
+		//
+		// ///
+		// }
+
+		ExecutorService service = Executors.newCachedThreadPool();
 		
-		long total = 0;
-		
-		for (Record record : data) {
-			Reference ref = sequence.getRef(record);
+		List<Object[]> pairs = new ArrayList<>(MAX_PAIRS);
+
+		for (final Record record : data) {
+			final Reference ref = sequence.getRef(record);
 			
-			int sum = process(record, ref);
+			pairs.add(new Object[] {record, ref});
 			
-			total += sum;
+			if (pairs.size() < MAX_PAIRS) {
+				continue;
+			}
 			
+			final List<Object[]> tmpPairs = pairs;
+			pairs = new ArrayList<>(MAX_PAIRS);
+
+			service.submit(new Runnable() {
+
+				@Override
+				public void run() {
+					
+					int sum = 0;
+					
+					for (Object[] objects : tmpPairs) {
+						Record rec = (Record) objects[0];
+						Reference ref = (Reference) objects[1];
+						sum += process(rec, ref);
+					}
+					
+					total.addAndGet(sum);
+				}
+			});
+
+			// total += sum;
+
 		}
-		
-		System.out.println(total);
-		
+
+		service.shutdown();
+
+		try {
+			service.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(total.get());
+
 	}
 
 	private int process(final Record record, final Reference ref) {
@@ -39,6 +99,5 @@ public class Metrics {
 		}
 		return sum;
 	}
-	
 
 }
