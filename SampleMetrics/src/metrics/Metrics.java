4ca12aad4d509a53fcefd29f4fb173bb834062bc
Zaal <Zaal_Lyanov@epam.com>
2016-06-08 17:25:19 +0300

    try with streams

--- SampleMetrics/src/metrics/Metrics.java
+++ SampleMetrics/src/metrics/Metrics.java
@@ -5,8 +5,13 @@ import java.time.Instant;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Spliterator;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class Metrics {
 
@@ -26,6 +31,8 @@ public class Metrics {
 
 	}
 
+	private AtomicInteger count = new AtomicInteger();
+
 	private void doWork() {
 
 		Data data = new Data();
@@ -39,32 +46,40 @@ public class Metrics {
 		// // do something
 		// }
 
-		List<CompletableFuture<Integer>> futures = new LinkedList<CompletableFuture<Integer>>();
-		
-		Semaphore sem = new Semaphore(8);
-		
-		for (Record record : data) {
-			Reference ref = sequence.getRef(record);
+//		List<CompletableFuture<Integer>> futures = new LinkedList<CompletableFuture<Integer>>();
+//
+//		Semaphore sem = new Semaphore(8);
 
-			sem.acquireUninterruptibly();
-			CompletableFuture<Integer> processTask = CompletableFuture
-					.supplyAsync(() -> {
-						int sum = process(record, ref);
-						sem.release();
-						return sum;
-					});
-						
-			
-			futures.add(processTask);
+		Stream<Record> stream = StreamSupport.stream(data.spliterator(), true);
 
-		}
+		total = stream.mapToInt(rec -> {
+			Reference ref = sequence.getRef(rec);
+			return process(rec, ref);
+		}).sum();
 		
-		for (CompletableFuture<Integer> completableFuture : futures) {
-			total += completableFuture.join();
-		}
-		
-		
+//		for (CompletableFuture<Integer> completableFuture : futures2) {
+//			total += completableFuture.join();
+//		}
 
+//		for (Record record : data) {
+//			Reference ref = sequence.getRef(record);
+//
+//			sem.acquireUninterruptibly();
+//			CompletableFuture<Integer> processTask = CompletableFuture
+//					.supplyAsync(() -> {
+//						int sum = process(record, ref);
+//						sem.release();
+//						return sum;
+//					});
+//
+//			futures.add(processTask);
+//
+//		}
+//
+//		for (CompletableFuture<Integer> completableFuture : futures) {
+//			total += completableFuture.join();
+//		}
+//
 		System.out.println(total);
 
 	}
@@ -72,14 +87,15 @@ public class Metrics {
 	private int process(final Record record, final Reference ref) {
 		int sum = 0;
 		try {
-			Thread.sleep(1);
+			Thread.sleep(0, 100);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		for (int i = 0; i < record.read.length; i++) {
 			sum += record.read[i];
-//			sum += ref.chromosome[i];
+			// sum += ref.chromosome[i];
 		}
+		System.out.println(count.incrementAndGet());
 		return sum;
 	}
 
